d635ab3356d2656784d5232d7aa3215aa4d023dd
Saintrick-FM <francymalonga8@gmail.com>
2024-11-19 06:18:21 +0100

    feat(home):handling burger menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,25 @@ year.textContent = new Date().getFullYear();
 // Get last modified date of the document
 document.getElementById("lastModified").textContent =
   "Last modification: " + document.lastModified;
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const burgerMenu = document.querySelector(".burger-menu");
+    const navBoxes = document.querySelector(".nav-boxes");
+    const box1 = document.querySelector(".box1");
+    const box2 = document.querySelector(".box2");
+    const box3 = document.querySelector(".box3");
+    const box4 = document.querySelector(".box4");
+    const main = document.querySelector("main");
+  
+    burgerMenu.addEventListener("click", () => {
+      navBoxes.classList.toggle("close");
+      burgerMenu.classList.toggle("open");
+      box1.classList.toggle("close");
+      box2.classList.toggle("close");
+      box3.classList.toggle("close");
+      box4.classList.toggle("close");
+      main.classList.toggle("go-up");
+      
+    });
+  });
+  
