b6721a9c3094e895a5d36c946116fd3b048d5816
Saintrick-FM <francymalonga8@gmail.com>
2024-11-28 19:18:01 +0100

    feat(chamber-home): small changes

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,26 +1,23 @@
+// Dynamically update copyright year
+let year = document.getElementById("year");
+year.textContent =new Date().getFullYear();
 
-
+// Get last modified date of the document
+document.getElementById("lastModified").textContent =
+  "Last modification: " + document.lastModified;
 
 document.addEventListener("DOMContentLoaded", () => {
-    const nav = document.querySelector("nav");
-    const burgerMenu = document.querySelector(".burger-menu");
-   
+  const nav = document.querySelector("nav");
+  const burgerMenu = document.querySelector(".burger-menu");
 
-    burgerMenu.addEventListener("click", () => {
-        nav.classList.toggle("close");
-      burgerMenu.classList.toggle("open");
+  burgerMenu.addEventListener("click", () => {
+    nav.classList.toggle("close");
+    burgerMenu.classList.toggle("open");
 
-
-if (nav.classList.contains("close"))  {
-
-    nav.style.display="flex"
-   
-    
-}else{
-    
-    nav.style.display="none"
-   
-
-}
-    });
+    if (nav.classList.contains("close")) {
+      nav.style.display = "flex";
+    } else {
+      nav.style.display = "none";
+    }
   });
+});
